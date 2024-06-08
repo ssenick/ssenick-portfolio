44f01ab947bb8a4eade82c6f3d5721c6317c9455
ssenick <ssenick@ukr.net>
2024-06-08 12:02:26 -0600

    create useDimensions.ts

---
+++ src/Layout/ui/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import { memo, ReactNode } from 'react';
+
+import { Header } from '@/components/Header';
+import { classNames } from '@/helpers/classNames/classNames';
+
+import cls from './Layout.module.scss';
+
+interface LayoutProps {
+   className?: string;
+   children?: ReactNode;
+}
+
+const Layout = memo((props: LayoutProps) => {
+   const { className, children } = props;
+
+   return (
+      <div className={classNames(cls.Layout, {}, [className])}>
+         <Header className={'app__header'} />
+         {children}
+      </div>
+   );
+});
+
+Layout.displayName = 'Layout';
+export { Layout };
---
+++ src/hooks/useDimensions.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export interface Dimensions {
+   width: number;
+   height: number;
+}
+
+export function useDimensions(): Dimensions {
+   const [dimensions, setDimensions] = useState<Dimensions>({ width: 0, height: 0 });
+
+   useEffect(() => {
+      const resize = () => {
+         setDimensions({
+            width: window.innerWidth,
+            height: window.innerHeight,
+         });
+      };
+      resize();
+      window.addEventListener('resize', resize);
+      return () => window.removeEventListener('resize', resize);
+   }, []);
+
+   return dimensions;
+}
