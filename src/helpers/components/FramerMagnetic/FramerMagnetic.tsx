30dfeca9b06ffd3739075e4fb309ddc91b0e268e
ssenick <ssenick@ukr.net>
2024-06-10 22:41:08 -0600

    improvement, added useCallback

--- src/helpers/components/FramerMagnetic/FramerMagnetic.tsx
+++ src/helpers/components/FramerMagnetic/FramerMagnetic.tsx
@@ -1,6 +1,15 @@
 import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion';
 import type { PropsWithChildren } from 'react';
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
+
+const dampen = 40; // Коэффициент демпфирования для эффекта
+const springConfig = {
+   damping: 15,
+   type: 'spring',
+   stiffness: 300,
+   mass: 0.8,
+   bounce: 0.5,
+}; // Конфигурация пружины
 
 const FramerMagnetic = (props: PropsWithChildren) => {
    const { children } = props;
@@ -8,37 +17,31 @@ const FramerMagnetic = (props: PropsWithChildren) => {
    const mouseX = useMotionValue(0);
    const mouseY = useMotionValue(0);
 
-   const dampen = 40; // Коэффициент демпфирования для эффекта
-   const springConfig = {
-      damping: 15,
-      type: 'spring',
-      stiffness: 300,
-      mass: 0.8,
-      bounce: 0.5,
-   }; // Конфигурация пружины
-
    const springX = useSpring(mouseX, springConfig);
    const springY = useSpring(mouseY, springConfig);
 
    const translateX = useTransform(springX, [-1, 1], [-dampen, dampen]);
    const translateY = useTransform(springY, [-1, 1], [-dampen, dampen]);
 
-   const handleMouseMove = (event) => {
-      if (!linkRef.current) return;
-      const rect = linkRef.current!.getBoundingClientRect();
-      const linkX = rect.left + rect.width / 2;
-      const linkY = rect.top + rect.height / 2;
-      const offsetX = (event.clientX - linkX) / rect.width;
-      const offsetY = (event.clientY - linkY) / rect.height;
+   const handleMouseMove = useCallback(
+      (event) => {
+         if (!linkRef.current) return;
+         const rect = linkRef.current!.getBoundingClientRect();
+         const linkX = rect.left + rect.width / 2;
+         const linkY = rect.top + rect.height / 2;
+         const offsetX = (event.clientX - linkX) / rect.width;
+         const offsetY = (event.clientY - linkY) / rect.height;
 
-      mouseX.set(offsetX);
-      mouseY.set(offsetY);
-   };
+         mouseX.set(offsetX);
+         mouseY.set(offsetY);
+      },
+      [mouseX, mouseY],
+   );
 
-   const handleMouseLeave = () => {
+   const handleMouseLeave = useCallback(() => {
       mouseX.set(0);
       mouseY.set(0);
-   };
+   }, [mouseX, mouseY]);
 
    return (
       <motion.div
