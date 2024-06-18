feb21acd4217ad8b9762f19be76e04c8914ac17e
ssenick <ssenick@ukr.net>
2024-06-17 21:00:50 -0600

    create AppButton

---
+++ src/components/UI/AppButton/AppButton.tsx
@@ -0,0 +1,65 @@
+import { motion } from 'framer-motion';
+import type { ButtonHTMLAttributes } from 'react';
+
+import { classNames } from '@/helpers/classNames/classNames';
+import { useFramerMagnetic } from '@/hooks/useFramerMagnetic';
+
+import cls from './AppButton.module.scss';
+
+type ButtonVariant = 'clear' | 'burger';
+
+interface AppButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+   className?: string;
+   variant?: ButtonVariant;
+   btnActive?: boolean;
+}
+const AppButton = (props: AppButtonProps) => {
+   const { className, btnActive, variant = 'clear', children, ...otherProps } = props;
+   const {
+      linkRef,
+      translateY,
+      translateX,
+      translateChildrenX,
+      translateChildrenY,
+      handleMouseLeave,
+      handleMouseMove,
+   } = useFramerMagnetic<HTMLDivElement>({
+      configChildrenEl: { dampingChildren: 30, stiffnessChildren: 350, dampenChildren: 30 },
+   });
+
+   return (
+      <button
+         className={classNames(cls.AppButton, { [cls.active]: btnActive }, [className, cls[variant]])}
+         {...otherProps}
+      >
+         <motion.div
+            className={cls.Button}
+            ref={linkRef}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            style={{
+               translateX: translateX,
+               translateY: translateY,
+               cursor: 'pointer',
+            }}
+         >
+            <motion.div
+               className={classNames(cls.children, {}, [])}
+               ref={linkRef}
+               onMouseMove={handleMouseMove}
+               onMouseLeave={handleMouseLeave}
+               style={{
+                  translateX: translateChildrenX,
+                  translateY: translateChildrenY,
+               }}
+            >
+               {children}
+               {variant === 'burger' && <div className={cls.burger} />}
+            </motion.div>
+         </motion.div>
+      </button>
+   );
+};
+
+AppButton.displayName = 'AppButton';
+export { AppButton };
