4a93a464af39e4389cd6122d5e8f5905b2e36ba7
ssenick <ssenick@ukr.net>
2024-06-20 22:38:46 -0600

    animation display, depending on the browser

--- src/Layout/Header/ui/NavAside/Curve/Curve.tsx
+++ src/Layout/Header/ui/NavAside/Curve/Curve.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 
 import { animatePattern } from '@/helpers/func/animatePattern';
+import { useBrowserInfo } from '@/hooks/useBrowserInfo';
 
 import cls from './Curve.module.scss';
 
@@ -25,8 +26,37 @@ const curve = {
       transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
    },
 };
+const curveSafari = {
+   initial: {
+      x: 0,
+      scaleY: 1,
+   },
+
+   animate: {
+      x: 100,
+      scaleY: 2.5,
+
+      transition: { duration: 0.9, ease: [0.76, 0, 0.24, 1] },
+   },
+
+   exit: {
+      x: 0,
+      scaleY: 1,
+
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+   },
+};
 
 const Curve = () => {
+   const browserInfo = useBrowserInfo();
+
+   if (browserInfo?.name === 'Safari') {
+      return (
+         <motion.svg {...animatePattern(curveSafari)} className={cls.CurveSafari}>
+            <path d={initialPath}></path>
+         </motion.svg>
+      );
+   }
    return (
       <svg className={cls.Curve}>
          <motion.path {...animatePattern(curve)}></motion.path>
