cb7cc32b9c505d9f12a0b3468bbc971673d1cb4c
ssenick <ssenick@ukr.net>
2024-06-25 22:22:29 -0600

    test create first screen

--- src/pages/HomePage/ui/HomePage/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage/HomePage.tsx
@@ -1,39 +1,41 @@
-import { useCallback, useState } from 'react';
-
-import ReactImage from '@/assets/react.svg?react';
-import ViteImage from '@/assets/vite.svg?react';
+import { useTheme } from '@/app/poviders/ThemeProvider';
+import ImageFront from '@/assets/front.svg?react';
+import ImageMe from '@/assets/me_3.png';
 import { classNames } from '@/helpers/classNames/classNames';
+import { useMediaQueryValues } from '@/hooks/useMediaQueryValues';
+import { Header } from '@/Layout';
+import { AppImage } from '@/UI/AppImage/AppImage';
 
 import cls from './HomePage.module.scss';
-
 interface HomePageProps {
    className?: string;
 }
 
 const HomePage = (props: HomePageProps) => {
    const { className } = props;
-   const [count, setCount] = useState(0);
+   const { isMobile } = useMediaQueryValues();
 
-   const addition = useCallback(() => {
-      setCount((prevState) => ++prevState);
-   }, []);
-   const reset = useCallback(() => {
-      setCount(0);
-   }, []);
+   const { theme } = useTheme();
 
    return (
       <div className={classNames(cls.HomePage, {}, [className])}>
-         <div className={cls.box}>
-            <ReactImage className={classNames(cls.image, {}, [cls.react])} />
-            <h1>HOME</h1>
-            <ViteImage className={cls.image} />
+         <div className={cls.firstScreen}>
+            <Header />
+            <ImageFront className={classNames(cls.svg, { [cls.dark]: theme === 'dark' }, [])} />
+            <AppImage className={classNames(cls.image, { [cls.dark]: theme === 'dark' }, [])} src={ImageMe} />
+            <div className={cls.wrapper}>
+               <div className={cls.main}>
+                  <h5>Ruslan Senchenko</h5>
+                  <h1>Website and user interface designer</h1>
+                  <h3>
+                     Over the past 9 years, as an art director and designer, I’ve worked with big companies
+                     and up-and-coming startups to successfully help them reach their full potential and
+                     attract new customers.
+                  </h3>
+               </div>
+            </div>
          </div>
-
-         <h2>{count}</h2>
-         <div className={cls.wrapper}>
-            <button onClick={addition}>+</button>
-            <button onClick={reset}>reset</button>
-         </div>
+         <div style={{ backgroundColor: 'red', height: '100vh' }}></div>
       </div>
    );
 };
