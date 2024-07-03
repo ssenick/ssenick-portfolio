b1ef0346690eeb721dbfe4091a751e09ffd7da6a
ssenick <ssenick@ukr.net>
2024-07-02 21:23:08 -0600

    add in opacity delay

--- src/const/animate.ts
+++ src/const/animate.ts
@@ -15,7 +15,7 @@ export const slide = {
 export const opacity = {
    initial: { opacity: 0 },
    animate: { opacity: 1 },
-   exit: { opacity: 0 },
+   exit: { opacity: 0, transition: { delay: 0.5 } },
 };
 
 export const scale = {
