d441b0dd2671f5f4145e2550881ab27e84dcc60c
ssenick <ssenick@ukr.net>
2024-07-08 20:29:25 -0600

    added scroll animation (not finish)

--- src/pages/HomePage/ui/HomeFirstScreen/HomeFirstScreen.tsx
+++ src/pages/HomePage/ui/HomeFirstScreen/HomeFirstScreen.tsx
@@ -1,10 +1,10 @@
-import { memo } from 'react';
+import { motion, useMotionValueEvent, useScroll, useTransform } from 'framer-motion';
+import { memo, useRef } from 'react';
 
 import { useTheme } from '@/app/poviders/ThemeProvider';
 import ImageFront from '@/assets/front.svg?react';
 import ImageMe from '@/assets/me_3.png';
 import { classNames } from '@/helpers/classNames/classNames';
-import { useVhVariable } from '@/hooks/useVhVariable';
 import { Header } from '@/Layout';
 import { AppImage } from '@/UI/AppImage/AppImage';
 
@@ -17,16 +17,34 @@ interface HomeFirstScreenProps {
 const HomeFirstScreen = memo((props: HomeFirstScreenProps) => {
    const { className } = props;
    const { theme } = useTheme();
+   const ref = useRef<HTMLDivElement | null>(null);
 
-   useVhVariable();
+   const { scrollYProgress } = useScroll({
+      target: ref,
+      offset: ['end end', 'end start'],
+   });
+
+   useMotionValueEvent(scrollYProgress, 'change', (latest) => {
+      console.log('x changed to', latest);
+   });
+
+   // const valueOpacity = useTransform(scrollYProgress, [0, 1], [1, 0.5]);
+   const valueMove = useTransform(scrollYProgress, [0, 1], ['0%', '13%']);
+   const valueMoveText = useTransform(scrollYProgress, [0, 1], ['0%', '25%']);
+
+   useMotionValueEvent(scrollYProgress, 'change', (latest) => {
+      console.log('x changed to', latest);
+   });
 
    return (
-      <div className={classNames(cls.HomeFirstScreen, {}, [className])}>
+      <div ref={ref} className={classNames(cls.HomeFirstScreen, {}, [className])}>
          <Header />
          <ImageFront className={classNames(cls.svg, { [cls.dark]: theme === 'dark' }, [])} />
-         <AppImage className={classNames(cls.image, { [cls.dark]: theme === 'dark' }, [])} src={ImageMe} />
+         <motion.div style={{ y: valueMove }} className={cls.image}>
+            <AppImage className={classNames(cls.img, { [cls.dark]: theme === 'dark' }, [])} src={ImageMe} />
+         </motion.div>
          <div className={cls.wrapper}>
-            <div className={cls.main}>
+            <motion.div style={{ y: valueMoveText }} className={cls.main}>
                <h5>Ruslan Senchenko</h5>
                <h1>Frontend Developer</h1>
                <h3>
@@ -34,7 +52,7 @@ const HomeFirstScreen = memo((props: HomeFirstScreenProps) => {
                   up-and-coming startups to successfully help them reach their full potential and attract new
                   customers.
                </h3>
-            </div>
+            </motion.div>
          </div>
       </div>
    );
