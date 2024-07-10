68abb6b87b5d614af8c96c07008df944cf3cfd40
ssenick <ssenick@ukr.net>
2024-07-09 21:55:27 -0600

    created component

---
+++ src/pages/HomePage/ui/TextSlider/TextSlider.tsx
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion';
+import { memo } from 'react';
+
+import ImageFront from '@/assets/front.svg?react';
+import { classNames } from '@/helpers/classNames/classNames';
+import { useParallaxSlider } from '@/hooks/useParallaxSlider';
+
+import cls from './TextSlider.module.scss';
+interface TextSliderProps {
+   className?: string;
+}
+
+const TextSlider = memo((props: TextSliderProps) => {
+   const { className } = props;
+   const x = useParallaxSlider(10);
+
+   return (
+      <motion.div className={classNames(cls.TextSlider, {}, [className])} style={{ x }}>
+         <ImageFront className={cls.svg} />
+         <ImageFront className={cls.svg} />
+      </motion.div>
+   );
+});
+
+TextSlider.displayName = 'TextSlider';
+export { TextSlider };
