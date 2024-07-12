97e82000ef3ac3a268d0dcc954593d3e9f979db0
ssenick <ssenick@ukr.net>
2024-07-11 19:36:18 -0600

    added  first page animation

--- src/pages/HomePage/ui/HomePage/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage/HomePage.tsx
@@ -1,5 +1,9 @@
+import { motion } from 'framer-motion';
+
+import { pageEaseOut } from '@/const/animate';
 import { classNames } from '@/helpers/classNames/classNames';
-import { Footer } from '@/Layout/Footer/ui/Footer/Footer';
+import { animatePattern } from '@/helpers/func/animatePattern';
+import { Footer } from '@/Layout';
 
 import { HomeFirstScreen } from '../HomeFirstScreen/HomeFirstScreen';
 import cls from './HomePage.module.scss';
@@ -12,12 +16,11 @@ const HomePage = (props: HomePageProps) => {
    const { className } = props;
 
    return (
-      <div className={classNames(cls.HomePage, {}, [className])}>
+      <motion.div className={classNames(cls.HomePage, {}, [className])} {...animatePattern(pageEaseOut)}>
          <HomeFirstScreen />
-
          <div style={{ backgroundColor: '#fff', height: '100vh', position: 'relative', zIndex: '2' }}></div>
          <Footer />
-      </div>
+      </motion.div>
    );
 };
 
