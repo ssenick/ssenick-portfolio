c09b7e96e5bba8a07b03a12ce9367db75a317ce3
ssenick <ssenick@ukr.net>
2024-07-16 20:42:11 -0600

    deleted memo

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,17 +1,19 @@
 import { motion } from 'framer-motion';
 import type { PropsWithChildren } from 'react';
-import { memo } from 'react';
 
 import { pageEaseOut } from '@/const/animate';
 import { classNames } from '@/helpers/classNames/classNames';
 import { animatePattern } from '@/helpers/func/animatePattern';
 import { Header } from '@/Layout';
 import { Footer } from '@/Layout';
+
+import cls from './Page.module.scss';
+
 interface PageProps extends PropsWithChildren {
    className?: string;
 }
-import cls from './Page.module.scss';
-const Page = memo((props: PageProps) => {
+
+const Page = (props: PageProps) => {
    const { className, children } = props;
 
    return (
@@ -21,7 +23,7 @@ const Page = memo((props: PageProps) => {
          <Footer />
       </motion.div>
    );
-});
+};
 
 Page.displayName = 'Page';
 export { Page };
