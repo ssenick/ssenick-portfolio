883e4224bf38d151a2b171097da41af74fd8717b
ssenick <ssenick@ukr.net>
2024-07-16 20:41:55 -0600

    added  HOME

--- src/Layout/Header/const/navLinks.ts
+++ src/Layout/Header/const/navLinks.ts
@@ -1,8 +1,12 @@
-import { getRouteAbout, getRouteContact, getRouteWork } from '@/config/route/routeConfig';
+import { getRouteAbout, getRouteContact, getRouteHome, getRouteWork } from '@/config/route/routeConfig';
 
 import type { NavLinksType } from '../types/types';
 
 export const navLinks: NavLinksType[] = [
+   {
+      path: getRouteHome(),
+      text: 'Home',
+   },
    {
       path: getRouteAbout(),
       text: 'About',
