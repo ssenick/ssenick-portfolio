528005c4138b4919b6fce241933957d40be9ef20
ssenick <ssenick@ukr.net>
2024-07-23 21:49:12 -0600

    created component

---
+++ src/components/AnimateList/ui/AnimateList/AnimateList.tsx
@@ -0,0 +1,39 @@
+import { memo, useState } from 'react';
+
+import { classNames } from '@/helpers/classNames/classNames';
+
+import type { animateListItemsType, animateModalType } from '../../model/types';
+import { AnimateListItem } from '../AnimateListItem/AnimateListItem';
+import { AnimateModal } from '../AnimateModal/AnimateModal';
+import cls from './AnimateList.module.scss';
+
+interface WorkListProps {
+   className?: string;
+   links: animateListItemsType[];
+}
+
+//  перенести весь конент в отдельный файлн
+
+const AnimateList = memo((props: WorkListProps) => {
+   const { className, links } = props;
+   const [modal, setModal] = useState<animateModalType>({ active: false, index: 0 });
+
+   return (
+      <ul className={classNames(cls.AnimateList, {}, [className])}>
+         <li className={cls.row}>
+            <div className={cls.link}>
+               <p>Name</p>
+               <p>Category</p>
+               <p>Year</p>
+            </div>
+         </li>
+         {links.map((item, i) => (
+            <AnimateListItem key={i + item.name} content={item} setModal={setModal} index={i} />
+         ))}
+         <AnimateModal modal={modal} links={links} />
+      </ul>
+   );
+});
+
+AnimateList.displayName = 'AnimateList';
+export { AnimateList };
