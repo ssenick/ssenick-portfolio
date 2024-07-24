6350f0db0d20667878f3b891499329e8819184b6
ssenick <ssenick@ukr.net>
2024-07-23 21:48:29 -0600

    added scaleAnimation animation

--- src/const/animate.ts
+++ src/const/animate.ts
@@ -66,3 +66,11 @@ export const pageEaseOut100 = {
    initial: { top: '100vh', opacity: 0 },
    animate: { top: 0, opacity: 1, transition: { duration: 1, ease: 'easeOut' } },
 };
+
+export const scaleAnimation = {
+   initial: { scale: 0, x: '-50%', y: '-50%' },
+
+   enter: { scale: 1, x: '-50%', y: '-50%', transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] } },
+
+   closed: { scale: 0, x: '-50%', y: '-50%', transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] } },
+};
