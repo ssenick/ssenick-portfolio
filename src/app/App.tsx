0828a1a9682e818abf7af3d71f1375971f898231
ssenick <ssenick@ukr.net>
2024-07-29 20:31:35 -0600

    added Suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,21 @@
 import './styles/index.scss';
 
+import { Suspense } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { PageTransition } from '@/app/poviders/PageTransition';
 import { AppRouter } from '@/app/poviders/Router';
 import { classNames } from '@/helpers/classNames/classNames';
-
 function App() {
    const location = useLocation();
 
    return (
       <div className={classNames('app', {}, [])}>
-         <PageTransition location={location}>
-            <AppRouter location={location} />
-         </PageTransition>
+         <Suspense fallback={'Loading...'}>
+            <PageTransition location={location}>
+               <AppRouter location={location} />
+            </PageTransition>
+         </Suspense>
       </div>
    );
 }
