c85b48edbce9f469859ee6d9e7a0a1d2ec17b498
ssenick <ssenick@ukr.net>
2024-07-30 20:41:00 -0600

    changed props type

--- src/helpers/components/FramerMagnetic/FramerMagnetic.tsx
+++ src/helpers/components/FramerMagnetic/FramerMagnetic.tsx
@@ -3,13 +3,18 @@ import type { PropsWithChildren } from 'react';
 
 import { useFramerMagnetic } from '@/hooks/useFramerMagnetic';
 
-const FramerMagnetic = (props: PropsWithChildren) => {
-   const { children } = props;
+interface FramerMagneticProps extends PropsWithChildren {
+   className?: string;
+}
+
+const FramerMagnetic = (props: FramerMagneticProps) => {
+   const { children, className } = props;
    const { linkRef, translateY, translateX, handleMouseLeave, handleMouseMove } =
       useFramerMagnetic<HTMLDivElement>({});
 
    return (
       <motion.div
+         className={className}
          ref={linkRef}
          onMouseMove={handleMouseMove}
          onMouseLeave={handleMouseLeave}
