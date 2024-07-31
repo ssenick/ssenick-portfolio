f789df9edf9da06cafe3d78b52a38b0f6e399d03
ssenick <ssenick@ukr.net>
2024-07-30 20:42:57 -0600

    added i18n config

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import './app/styles/index.scss';
+import '@/app/styles/index.scss';
+import '@/config/i18n/i18n';
 
 import { ReactLenis } from '@studio-freight/react-lenis';
 import { StrictMode } from 'react';
@@ -7,7 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/app/App.tsx';
 import { MediaQueryProvider } from '@/app/poviders/MediaQueryProvider';
-
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to mount the application, check the container!');
 
