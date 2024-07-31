fbe58a3d72f9fa8c3c490fde51cc6b74b4df8ef3
ssenick <ssenick@ukr.net>
2024-07-30 20:42:35 -0600

    created config i18n

---
+++ src/config/i18n/i18n.ts
@@ -0,0 +1,28 @@
+import i18n from 'i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+import { LANGUAGE_LOCALSTORAGE_KEY } from '@/const/localstorage';
+const initialLanguage = 'en';
+
+if (!localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY)) {
+   localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, initialLanguage);
+}
+void i18n
+   .use(Backend)
+   // .use(LanguageDetector)
+   .use(initReactI18next)
+   .init({
+      lng: localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY) ?? initialLanguage,
+      fallbackLng: localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY) ?? initialLanguage,
+      debug: __IS_DEV__,
+      backend: {
+         loadPath: '/locales/{{lng}}/{{ns}}.json',
+      },
+      interpolation: {
+         escapeValue: false, // not needed for react as it escapes by default
+      },
+   });
+
+export default i18n;
