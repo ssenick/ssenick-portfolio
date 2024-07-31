feaea0d3569c8284f0b85e81fd8b29127222148e
ssenick <ssenick@ukr.net>
2024-07-30 20:39:28 -0600

    added translation

--- src/Layout/Header/ui/MenuBtn/MenuBtn.tsx
+++ src/Layout/Header/ui/MenuBtn/MenuBtn.tsx
@@ -1,21 +1,24 @@
 import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
 
+import { classNames } from '@/helpers/classNames/classNames';
 import { AppButton } from '@/UI/AppButton/AppButton';
 
 interface MenuBtnProps {
    setActiveButton: (activeButton: boolean) => void;
+   className?: string;
 }
 
 const MenuBtn = memo((props: MenuBtnProps) => {
-   const { setActiveButton } = props;
-
+   const { setActiveButton, className } = props;
+   const { t } = useTranslation();
    const toggleButton = useCallback(() => {
       setActiveButton(true);
    }, [setActiveButton]);
 
    return (
-      <AppButton onClick={toggleButton} variant="menu">
-         Menu
+      <AppButton onClick={toggleButton} variant="menu" className={classNames('', {}, [className])}>
+         {t('Menu')}
       </AppButton>
    );
 });
