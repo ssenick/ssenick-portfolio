997e2e7157b1d5341793e053163bb676f90581a4
ssenick <ssenick@ukr.net>
2024-07-30 20:43:39 -0600

    move to a separate file languages

---
+++ src/components/LangSwitcher/model/languages.tsx
@@ -0,0 +1,14 @@
+import IconEN from '@/assets/flag_en.svg?react';
+import IconRU from '@/assets/flag_ru.svg?react';
+import IconSK from '@/assets/flag_sk.svg?react';
+import IconUA from '@/assets/flag_ua.svg?react';
+import type { ListBoxItem } from '@/UI/ListBox/ListBox';
+
+import { Languages } from '../model/types';
+
+export const languages: ListBoxItem<Languages>[] = [
+   { value: Languages.EN, content: { text: 'EN', icon: <IconEN /> } },
+   { value: Languages.RU, content: { text: 'RU', icon: <IconRU /> } },
+   { value: Languages.UA, content: { text: 'UA', icon: <IconUA /> } },
+   { value: Languages.SK, content: { text: 'SK', icon: <IconSK /> } },
+];
