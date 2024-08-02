f44f744be39cc74e6ccab376ee64a5c7c8e7ff3a
ssenick <ssenick@ukr.net>
2024-08-01 20:47:21 -0600

    deleted fallback in Suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,15 @@ import './styles/index.scss';
 import { Suspense } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { PageTransition } from '@/app/poviders/PageTransition';
-import { AppRouter } from '@/app/poviders/Router';
 import { classNames } from '@/helpers/classNames/classNames';
+import { PageTransition } from '@/poviders/PageTransition';
+import { AppRouter } from '@/poviders/Router';
 function App() {
    const location = useLocation();
 
    return (
       <div className={classNames('app', {}, [])}>
-         <Suspense fallback={'Loading...'}>
+         <Suspense fallback={''}>
             <PageTransition location={location}>
                <AppRouter location={location} />
             </PageTransition>
