e03015a47ac9dffeacd56071559bbcb91ef51edb
ssenick <ssenick@ukr.net>
2024-08-02 20:05:18 -0600

    added videos variable

--- src/components/ParallaxSlider/ParallaxSlider.tsx
+++ src/components/ParallaxSlider/ParallaxSlider.tsx
@@ -2,21 +2,25 @@ import type { MotionValue } from 'framer-motion';
 import { motion, useTransform } from 'framer-motion';
 import { memo, useMemo } from 'react';
 
+import ErrorImage from '@/assets/errorImage.svg';
 import { AppImage } from '@/components/UI/AppImage/AppImage';
+import { AppVideo } from '@/components/UI/AppVideo/AppVideo';
 import { classNames } from '@/helpers/classNames/classNames';
+import { useBrowserInfo } from '@/hooks/useBrowserInfo';
 
 import cls from './ParallaxSlider.module.scss';
 interface ParallaxSliderProps {
    className?: string;
-   images: string[];
+   images?: string[];
+   videos?: string[];
    progress: MotionValue<number>;
    direction?: 'left' | 'right';
    revers?: boolean;
 }
 
 const ParallaxSlider = memo((props: ParallaxSliderProps) => {
-   const { className, images, progress, direction = 'left', revers } = props;
-
+   const { className, images, videos, progress, direction = 'left', revers } = props;
+   const browser = useBrowserInfo();
    const handlerDirection = useMemo(() => (direction === 'left' ? -1 : 1), [direction]);
    const translateX = useTransform(
       progress,
@@ -29,16 +33,28 @@ const ParallaxSlider = memo((props: ParallaxSliderProps) => {
          className={classNames(cls.ParallaxSlider, { [cls.revers]: revers }, [className])}
          style={{ x: translateX }}
       >
-         {images.map((image) => (
+         {images?.map((image) => (
             <div key={image} className={cls.image}>
                <AppImage src={image} className={cls.img} />
             </div>
          ))}
-         {/*{images.map((image) => (*/}
-         {/*   <div key={image} className={cls.image}>*/}
-         {/*      <video src={image} autoPlay className={cls.img} playsInline muted loop></video>*/}
-         {/*   </div>*/}
-         {/*))}*/}
+         {!images &&
+            videos?.map((video) => (
+               <div key={video} className={cls.image}>
+                  {/*<video className={cls.img} src={video} autoPlay muted loop></video>*/}
+                  <AppVideo
+                     className={cls.img}
+                     src={video}
+                     spare={<div className={cls.spare}></div>}
+                     errorSpare={<img src={ErrorImage} className={cls.img} alt="error" />}
+                     autoPlay
+                     playsInline
+                     muted
+                     loop={browser?.name !== 'Safari'}
+                     preload="auto"
+                  />
+               </div>
+            ))}
       </motion.div>
    );
 });
