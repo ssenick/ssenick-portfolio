69c8e97d34b547a3117ba7bbaf8b150ae3b7e922
ssenick <ssenick@ukr.net>
2024-08-05 15:53:08 -0600

    created types

---
+++ src/types/projectsItems.ts
@@ -0,0 +1,12 @@
+interface projectImages {
+   preview: string;
+}
+
+export interface projectType {
+   id: number;
+   name: string;
+   year: string;
+   src: string;
+   path: string;
+   images: projectImages;
+}
