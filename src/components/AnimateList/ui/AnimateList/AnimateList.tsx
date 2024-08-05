fbb6d1cb5d826a45be10f887eb079ff87dae2e4c
ssenick <ssenick@ukr.net>
2024-08-05 15:48:38 -0600

    added path, path type

--- src/components/AnimateList/ui/AnimateList/AnimateList.tsx
+++ src/components/AnimateList/ui/AnimateList/AnimateList.tsx
@@ -1,16 +1,18 @@
 import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { getRouteProject } from '@/config/route/routeConfig';
 import { classNames } from '@/helpers/classNames/classNames';
+import type { projectType } from '@/types/projectsItems';
 
-import type { animateListItemsType, animateModalType } from '../../model/types';
+import type { animateModalType } from '../../model/types';
 import { AnimateListItem } from '../AnimateListItem/AnimateListItem';
 import { AnimateModal } from '../AnimateModal/AnimateModal';
 import cls from './AnimateList.module.scss';
 
 interface WorkListProps {
    className?: string;
-   links: animateListItemsType[];
+   links: projectType[];
    header?: boolean;
 }
 
@@ -32,7 +34,13 @@ const AnimateList = memo((props: WorkListProps) => {
             </li>
          )}
          {links.map((item, i) => (
-            <AnimateListItem key={i + item.name} content={item} setModal={setModal} index={i} />
+            <AnimateListItem
+               key={item.id + item.name}
+               content={item}
+               setModal={setModal}
+               index={i}
+               path={getRouteProject(item.path)}
+            />
          ))}
          <AnimateModal modal={modal} links={links} />
       </ul>
