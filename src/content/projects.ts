9a5e2d278292f230add5b4c12c76aec552a6e32a
ssenick <ssenick@ukr.net>
2024-08-05 15:52:57 -0600

    added content

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -1,7 +1,31 @@
-import type { animateListItemsType } from '@/components/AnimateList';
+import AdminPreview from '@/assets/projects/admin/admin-small.jpg';
+import BlogPreview from '@/assets/projects/blog/blog-small.jpg';
+import GreenShopPreview from '@/assets/projects/greenshop/greenshop-small.jpg';
+import type { projectType } from '@/types/projectsItems';
 
-export const projects: animateListItemsType[] = [
-   { name: 'Blog', year: '2024', src: '' },
-   { name: 'Blog', year: '2024', src: '' },
-   { name: 'Blog', year: '2024', src: '' },
+export const projects: projectType[] = [
+   {
+      id: 1,
+      name: 'Blog',
+      year: '2024',
+      src: 'https://dulcet-pudding-009166.netlify.app',
+      path: 'blog',
+      images: { preview: BlogPreview },
+   },
+   {
+      id: 2,
+      name: 'Admin-panel',
+      year: '2024',
+      src: 'https://web-react-admin.ssenick.fun',
+      path: 'admin-panel',
+      images: { preview: AdminPreview },
+   },
+   {
+      id: 3,
+      name: 'Green-shop',
+      year: '2023',
+      src: 'https://greenshop.ssenick.fun/',
+      path: 'green-shop',
+      images: { preview: GreenShopPreview },
+   },
 ];
