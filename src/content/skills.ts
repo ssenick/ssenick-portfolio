99ee28f0cc37a526ab41513bc602a83e06e8c3e2
ssenick <ssenick@ukr.net>
2024-08-09 20:39:00 -0600

    created

---
+++ src/content/skills.ts
@@ -0,0 +1,29 @@
+import type { Skills } from '@/types/skills';
+
+export const skills: Skills = {
+   technicalSkills: [
+      { id: 1, name: 'JavaScript' },
+      { id: 2, name: 'TypeScript' },
+      { id: 3, name: 'React.js' },
+      { id: 4, name: 'Redux Toolkit' },
+      { id: 5, name: 'RTK Query' },
+      { id: 6, name: 'RESTful APIs' },
+      { id: 7, name: 'HTML' },
+      { id: 8, name: 'SCSS' },
+      { id: 9, name: 'Tailwind CSS' },
+      { id: 10, name: 'Responsive design' },
+      { id: 11, name: 'i18n' },
+      { id: 12, name: 'Methodology BEM' },
+   ],
+   tools: [
+      { id: 1, name: 'Git' },
+      { id: 2, name: 'Webpack' },
+      { id: 3, name: 'Vite' },
+      { id: 4, name: 'Storybook' },
+      { id: 5, name: 'Jest' },
+      { id: 6, name: 'ESLint' },
+      { id: 7, name: 'StyleLint' },
+      { id: 8, name: 'Prettier' },
+      { id: 9, name: 'CI/CD pipelines' },
+   ],
+};
---
+++ src/types/skills.ts
@@ -0,0 +1,9 @@
+interface SkillsType {
+   id: number;
+   name: string;
+}
+
+export interface Skills {
+   technicalSkills: SkillsType[];
+   tools: SkillsType[];
+}
