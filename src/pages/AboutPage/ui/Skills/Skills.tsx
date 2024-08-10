8c8817426002675891239fb1b3b86890181f9e66
ssenick <ssenick@ukr.net>
2024-08-09 20:39:35 -0600

    added

--- src/pages/AboutPage/ui/Skills/Skills.tsx
+++ src/pages/AboutPage/ui/Skills/Skills.tsx
@@ -1,6 +1,8 @@
 import { memo } from 'react';
 
 import { BlockWithTitle } from '@/components/BlockWithTitle/BlockWithTitle';
+import { Title } from '@/components/UI/Title/Title';
+import { skills } from '@/content/skills';
 import { classNames } from '@/helpers/classNames/classNames';
 
 import cls from './Skills.module.scss';
@@ -13,7 +15,30 @@ const Skills = memo((props: SkillsProps) => {
    const { className } = props;
 
    return (
-      <BlockWithTitle className={classNames(cls.Skills, {}, [className])} title={'skills'}></BlockWithTitle>
+      <BlockWithTitle className={classNames(cls.Skills, {}, [className])} title={'Knowledge Areas'}>
+         <div className={cls.content}>
+            <div className={cls.wrapper}>
+               <Title notPadding>Technical Skills:</Title>
+               <ul className={cls.list}>
+                  {skills.technicalSkills.map(({ name, id }) => (
+                     <li key={id} className={cls.item}>
+                        {name}
+                     </li>
+                  ))}
+               </ul>
+            </div>
+            <div className={cls.wrapper}>
+               <Title notPadding>Tools and Methodologies:</Title>
+               <ul className={cls.list}>
+                  {skills.tools.map(({ name, id }) => (
+                     <li key={id} className={cls.item}>
+                        {name}
+                     </li>
+                  ))}
+               </ul>
+            </div>
+         </div>
+      </BlockWithTitle>
    );
 });
 
