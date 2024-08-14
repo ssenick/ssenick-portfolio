23df426a0cf13258dd3a90f0b10f793edc3ac944
ssenick <ssenick@ukr.net>
2024-08-13 22:38:04 -0600

    removed animations

--- src/components/BlockWithTitle/BlockWithTitle.tsx
+++ src/components/BlockWithTitle/BlockWithTitle.tsx
@@ -1,8 +1,6 @@
-import { motion, useInView } from 'framer-motion';
 import type { PropsWithChildren } from 'react';
 import { memo, useRef } from 'react';
 
-import { moveToTop } from '@/const/animate';
 import { classNames } from '@/helpers/classNames/classNames';
 
 import cls from './BlockWithTitle.module.scss';
@@ -16,17 +14,11 @@ const BlockWithTitle = memo((props: BlockWithTitleProps) => {
    const { className, title, children } = props;
    const containerRef = useRef<HTMLDivElement | null>(null);
 
-   const isInView = useInView(containerRef);
    return (
-      <motion.div
-         ref={containerRef}
-         className={classNames(cls.BlockWithTitle, {}, [className])}
-         variants={moveToTop}
-         animate={isInView ? 'animate' : 'initial'}
-      >
+      <div ref={containerRef} className={classNames(cls.BlockWithTitle, {}, [className])}>
          {title && <h2 className={cls.title}>{title}</h2>}
          {children}
-      </motion.div>
+      </div>
    );
 });
 
