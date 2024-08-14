1e8f9c58fc74a68feec0d0adb74639cbe668a5d2
ssenick <ssenick@ukr.net>
2024-08-13 22:39:51 -0600

    added transitions

--- src/pages/AboutPage/ui/Skills/Skills.tsx
+++ src/pages/AboutPage/ui/Skills/Skills.tsx
@@ -1,4 +1,5 @@
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { BlockWithTitle } from '@/components/BlockWithTitle/BlockWithTitle';
 import { Title } from '@/components/UI/Title/Title';
@@ -13,12 +14,13 @@ interface SkillsProps {
 
 const Skills = memo((props: SkillsProps) => {
    const { className } = props;
+   const { t } = useTranslation('about');
 
    return (
-      <BlockWithTitle className={classNames(cls.Skills, {}, [className])} title={'Knowledge Areas'}>
+      <BlockWithTitle className={classNames(cls.Skills, {}, [className])} title={t('titleSkills')}>
          <div className={cls.content}>
             <div className={cls.wrapper}>
-               <Title notPadding>Technical Skills:</Title>
+               <Title notPadding>{t('firstSubtitleSkills')}</Title>
                <ul className={cls.list}>
                   {skills.technicalSkills.map(({ name, id }) => (
                      <li key={id} className={cls.item}>
@@ -28,7 +30,8 @@ const Skills = memo((props: SkillsProps) => {
                </ul>
             </div>
             <div className={cls.wrapper}>
-               <Title notPadding>Tools and Methodologies:</Title>
+               <Title notPadding>{t('secondSubtitleSkills')}</Title>
+
                <ul className={cls.list}>
                   {skills.tools.map(({ name, id }) => (
                      <li key={id} className={cls.item}>
