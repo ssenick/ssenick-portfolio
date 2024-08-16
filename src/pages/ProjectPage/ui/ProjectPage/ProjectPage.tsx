5aa37121ebdfe6415ec8b707047c4a5ee21c2181
ssenick <ssenick@ukr.net>
2024-08-15 20:31:25 -0600

    test variable

--- src/pages/ProjectPage/ui/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ui/ProjectPage/ProjectPage.tsx
@@ -1,9 +1,7 @@
 import { useParams } from 'react-router-dom';
 
 import { Page } from '@/components/Page/Page';
-import { projects } from '@/content/projects';
 import { classNames } from '@/helpers/classNames/classNames';
-import { NotFoundPage } from '@/pages/NotFoundPage';
 
 import cls from './ProjectPage.module.scss';
 
@@ -15,15 +13,12 @@ const ProjectPage = (props: ProjectPageProps) => {
    const { className } = props;
    const { name } = useParams<{ name: string }>();
 
-   const findName = projects.find((item) => item.path === name);
-   if (!findName) {
-      return <NotFoundPage />;
-   }
+   // const findName = projects.find((item) => item.path === name);
 
    return (
       <Page className={classNames(cls.ProjectPage, {}, [className])}>
          <div>
-            <div>BLog</div>
+            <div>{name}</div>
          </div>
       </Page>
    );
