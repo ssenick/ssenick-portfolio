f1b495a24fcca539292e318b8d71395d42a55a85
ssenick <ssenick@ukr.net>
2024-08-22 21:27:09 -0600

    adding ContactHeader

--- src/pages/ContactPage/ui/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ui/ContactPage/ContactPage.tsx
@@ -1,8 +1,9 @@
 import { Page } from '@/components/Page/Page';
 import { classNames } from '@/helpers/classNames/classNames';
 
-import { ContactMap } from '../ContactMap/ContactMap.tsx';
+import { ContactHeader } from '../ContactHeader/ContactHeader.tsx';
 import cls from './ContactPage.module.scss';
+
 interface AboutPageProps {
    className?: string;
 }
@@ -12,35 +13,7 @@ const ContactPage = (props: AboutPageProps) => {
 
    return (
       <Page className={classNames(cls.ContactPage, {}, [className])}>
-         <div className={cls.container}>
-            <div className={cls.header}>
-               <ContactMap className={cls.map} />
-               <div className={cls.title}>
-                  <i>L</i>
-                  <i>E</i>
-                  <i className={cls.apostrophe}>T</i>
-                  <i>S</i>
-                  <i>W</i>
-                  <i>O</i>
-                  <i>R</i>
-                  <i className={cls.mark}>K</i>
-                  <i>T</i>
-                  <i>O</i>
-                  <i>G</i>
-                  <i>E</i>
-                  <i>T</i>
-                  <i>H</i>
-                  <i>E</i>
-                  <i>R</i>
-               </div>
-            </div>
-
-            <div className={cls.wrapper}>
-               <h1>CONTACT</h1>
-
-               <div style={{ height: '100vh' }}></div>
-            </div>
-         </div>
+         <ContactHeader />
       </Page>
    );
 };
