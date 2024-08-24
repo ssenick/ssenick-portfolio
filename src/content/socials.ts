ba9e242aa31326138c9f82f74e74b88118e7e4b1
ssenick <ssenick@ukr.net>
2024-08-23 22:02:20 -0600

    renamed

--- src/content/socials.ts
+++ src/content/socials.ts
@@ -1,20 +1,24 @@
-import type { Socials } from '../Layout/Header/types/types';
+import type { ILinksList } from '@/components/LinksList/LinksList.tsx';
 
-export const socials: Socials[] = [
+export const socials: ILinksList[] = [
    {
       text: 'Instagram',
       href: 'https://www.instagram.com/senchenkoruslan',
+      target: '_blank',
    },
    {
       text: 'Facebook',
       href: 'https://www.facebook.com/profile.php?id=100003676742055',
+      target: '_blank',
    },
    {
       text: 'Linkedin',
       href: 'https://www.linkedin.com/in/ruslan-senchenko',
+      target: '_blank',
    },
    {
       text: 'GitHub',
       href: 'https://github.com/ssenick',
+      target: '_blank',
    },
 ];
