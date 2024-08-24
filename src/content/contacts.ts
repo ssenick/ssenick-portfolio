c1917f06ac4766586ccf6bd695babc3287b29330
ssenick <ssenick@ukr.net>
2024-08-23 22:05:32 -0600

    created

---
+++ src/content/contacts.ts
@@ -0,0 +1,14 @@
+import type { ILinksList } from '@/components/LinksList/LinksList.tsx';
+
+export const contacts: ILinksList[] = [
+   {
+      text: '+14037085691',
+      href: 'tel:+14037085691',
+      target: '_self',
+   },
+   {
+      text: 'ssenick@ukr.net',
+      href: 'mailto:ssenick@ukr.net',
+      target: '_self',
+   },
+];
