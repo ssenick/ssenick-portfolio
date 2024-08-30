0db89838924bd91bea717cd73bd8623a0fb6d9ae
ssenick <ssenick@ukr.net>
2024-08-29 22:39:45 -0600

    added AnimateList and translation hook

--- src/pages/WorkPage/ui/WorkPage/WorkPage.tsx
+++ src/pages/WorkPage/ui/WorkPage/WorkPage.tsx
@@ -1,4 +1,8 @@
+import { useTranslation } from 'react-i18next';
+
+import { AnimateList } from '@/components/AnimateList';
 import { Page } from '@/components/Page/Page';
+import { projects } from '@/content/projects.ts';
 import { classNames } from '@/helpers/classNames/classNames';
 
 import cls from './WorkPage.module.scss';
@@ -9,10 +13,13 @@ interface WorkPageProps {
 
 const WorkPage = (props: WorkPageProps) => {
    const { className } = props;
+   const { t } = useTranslation('work');
    return (
       <Page className={classNames(cls.WorkPage, {}, [className])}>
-         <h1>WORK</h1>
-         <div style={{ height: '200vh' }}></div>
+         <div className={cls.wrapper}>
+            <h1 className={cls.title}>{t('title')}</h1>
+            <AnimateList links={projects} header />
+         </div>
       </Page>
    );
 };
