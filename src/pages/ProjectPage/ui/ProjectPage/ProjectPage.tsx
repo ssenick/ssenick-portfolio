e11b1b3943eb838af9c412b3178df412dbabd4af
ssenick <ssenick@ukr.net>
2024-09-09 20:27:41 -0600

    fixed styles

--- src/pages/ProjectPage/ui/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ui/ProjectPage/ProjectPage.tsx
@@ -1,24 +1,32 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Page } from '@/components/Page/Page';
+import { projects } from '@/content/projects.ts';
 import { classNames } from '@/helpers/classNames/classNames';
+import { NotFoundPage } from '@/pages/NotFoundPage';
 
+import { ProjectHeader } from '../ProjectHeader/ProjectHeader.tsx';
 import cls from './ProjectPage.module.scss';
 
 interface ProjectPageProps {
    className?: string;
 }
-
+//
 const ProjectPage = (props: ProjectPageProps) => {
    const { className } = props;
    const { name } = useParams<{ name: string }>();
+   // Состояние для отсроченного удаления проекта
+   const [project] = useState(() => projects.find((item) => item.path === name));
 
-   // const findName = projects.find((item) => item.path === name);
+   if (!project) {
+      return <NotFoundPage />;
+   }
 
    return (
       <Page className={classNames(cls.ProjectPage, {}, [className])}>
-         <div>
-            <div>{name}</div>
+         <div className={cls.wrapper}>
+            <ProjectHeader project={project} />
          </div>
       </Page>
    );
