41691c5ee177bcc5287965c9012f6334354a13df
ssenick <ssenick@ukr.net>
2024-09-09 20:27:13 -0600

    added small font size

--- src/components/UI/Title/Title.tsx
+++ src/components/UI/Title/Title.tsx
@@ -8,13 +8,23 @@ interface TitleProps {
    className?: string;
    children?: ReactNode;
    notPadding?: boolean;
+   borderBottom?: boolean;
+   smallSize?: boolean;
 }
 
 const Title = memo((props: TitleProps) => {
-   const { className, children, notPadding } = props;
+   const { className, children, notPadding, borderBottom, smallSize } = props;
 
    return (
-      <h5 className={classNames(cls.Title, { [cls.notPadding]: notPadding }, [className])}>{children}</h5>
+      <h5
+         className={classNames(
+            cls.Title,
+            { [cls.notPadding]: notPadding, [cls.borderBottom]: borderBottom, [cls.smallSize]: smallSize },
+            [className],
+         )}
+      >
+         {children}
+      </h5>
    );
 });
 
