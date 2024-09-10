d8cbde76ef6fb455a52ca42fb8b0d3bb6e5f54ed
ssenick <ssenick@ukr.net>
2024-09-09 20:26:41 -0600

    added location, role, projectPath

--- src/types/projectsItems.ts
+++ src/types/projectsItems.ts
@@ -6,7 +6,9 @@ export interface projectType {
    id: number;
    name: string;
    year: string;
-   src: string;
+   location: string;
+   role: string;
+   projectPath: string;
    path: string;
    images: projectImages;
    technologies: string;
