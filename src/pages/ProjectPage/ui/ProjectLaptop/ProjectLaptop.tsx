884dbcc15db2f74b3dcab444772b34767299fc04
ssenick <ssenick@ukr.net>
2024-09-16 20:34:23 -0600

    changed Props

--- src/pages/ProjectPage/ui/ProjectLaptop/ProjectLaptop.tsx
+++ src/pages/ProjectPage/ui/ProjectLaptop/ProjectLaptop.tsx
@@ -3,21 +3,22 @@ import { memo } from 'react';
 import LaptopImage from '@/assets/devices/device-mbp-15.webp';
 import { AppImage } from '@/components/UI/AppImage/AppImage.tsx';
 import { classNames } from '@/helpers/classNames/classNames';
+import type { projectsImageSizes } from '@/types/projectsItems.ts';
 
 import cls from './ProjectLaptop.module.scss';
 
 interface ProjectLaptopProps {
    className?: string;
-   image: string;
+   images: projectsImageSizes;
 }
 
 const ProjectLaptop = memo((props: ProjectLaptopProps) => {
-   const { className, image } = props;
-
+   const { className, images } = props;
+   const { big, small } = images;
    return (
       <div className={classNames(cls.ProjectLaptop, {}, [className])}>
          <div className={cls.picture}>
-            <AppImage className={cls.image} src={image} alt="project image" loading="lazy" />
+            <AppImage className={cls.image} src={big} spare={small} alt="project image" loading="lazy" />
          </div>
          <div className={cls.device}>
             <AppImage className={cls.laptopImage} src={LaptopImage} alt="macbook image" />
