3fe410e4394eb413bea649feea0576df4ae44b96
ssenick <ssenick@ukr.net>
2024-09-16 20:34:26 -0600

    changed Props

--- src/pages/ProjectPage/ui/ProjectMobile/ProjectMobile.tsx
+++ src/pages/ProjectPage/ui/ProjectMobile/ProjectMobile.tsx
@@ -3,21 +3,22 @@ import { memo } from 'react';
 import MobileImage from '@/assets/devices/iphone.webp';
 import { AppImage } from '@/components/UI/AppImage/AppImage.tsx';
 import { classNames } from '@/helpers/classNames/classNames';
+import type { projectsImageSizes } from '@/types/projectsItems.ts';
 
 import cls from './ProjectMobile.module.scss';
 
 interface ProjectMobileProps {
    className?: string;
-   image: string;
+   images: projectsImageSizes;
 }
 
 const ProjectMobile = memo((props: ProjectMobileProps) => {
-   const { className, image } = props;
-
+   const { className, images } = props;
+   const { big, small } = images;
    return (
       <div className={classNames(cls.ProjectMobile, {}, [className])}>
          <div className={cls.picture}>
-            <AppImage className={cls.image} src={image} alt="project image" loading="lazy" />
+            <AppImage className={cls.image} src={big} spare={small} alt="project image" loading="lazy" />
          </div>
          <div className={cls.device}>
             <AppImage className={cls.laptopImage} src={MobileImage} alt="mobile image" />
