f769c9c7aa5b690e8b43f0497240bcacf7f8297b
ssenick <ssenick@ukr.net>
2024-09-17 22:03:18 -0600

    added tablet props

--- src/pages/ProjectPage/ui/ProjectLaptop/ProjectLaptop.tsx
+++ src/pages/ProjectPage/ui/ProjectLaptop/ProjectLaptop.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 
 import LaptopImage from '@/assets/devices/device-mbp-15.webp';
+import TabletImage from '@/assets/devices/tablet.webp';
 import { AppImage } from '@/components/UI/AppImage/AppImage.tsx';
 import { classNames } from '@/helpers/classNames/classNames';
 import type { projectsImageSizes } from '@/types/projectsItems.ts';
@@ -10,10 +11,11 @@ import cls from './ProjectLaptop.module.scss';
 interface ProjectLaptopProps {
    className?: string;
    images: projectsImageSizes;
+   tablet?: boolean;
 }
 
 const ProjectLaptop = memo((props: ProjectLaptopProps) => {
-   const { className, images } = props;
+   const { className, images, tablet } = props;
    const { big, small } = images;
    return (
       <div className={classNames(cls.ProjectLaptop, {}, [className])}>
@@ -21,7 +23,11 @@ const ProjectLaptop = memo((props: ProjectLaptopProps) => {
             <AppImage className={cls.image} src={big} spare={small} alt="project image" loading="lazy" />
          </div>
          <div className={cls.device}>
-            <AppImage className={cls.laptopImage} src={LaptopImage} alt="macbook image" />
+            <AppImage
+               className={cls.laptopImage}
+               src={tablet ? TabletImage : LaptopImage}
+               alt="macbook image"
+            />
          </div>
       </div>
    );
