23985677816a9ac9351a58ed90ca1e421ba56796
ssenick <ssenick@ukr.net>
2024-09-17 22:04:50 -0600

    test layout

--- src/pages/ProjectPage/ui/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ui/ProjectPage/ProjectPage.tsx
@@ -5,10 +5,10 @@ import { Page } from '@/components/Page/Page';
 import { projects } from '@/content/projects.ts';
 import { classNames } from '@/helpers/classNames/classNames';
 import { NotFoundPage } from '@/pages/NotFoundPage';
-import { ProjectLaptop } from '@/pages/ProjectPage/ui/ProjectLaptop/ProjectLaptop.tsx';
 
 import { ProjectDevices } from '../ProjectDevices/ProjectDevices.tsx';
 import { ProjectHeader } from '../ProjectHeader/ProjectHeader.tsx';
+import { ProjectLaptop } from '../ProjectLaptop/ProjectLaptop.tsx';
 import cls from './ProjectPage.module.scss';
 
 interface ProjectPageProps {
@@ -24,15 +24,20 @@ const ProjectPage = (props: ProjectPageProps) => {
    if (!project) {
       return <NotFoundPage />;
    }
+
    const laptopImages = project?.images?.devices?.laptop;
+   const mainImages = project?.images?.devices?.tablet;
+   const mobileImages = project?.images?.devices?.mobile;
    return (
       <Page className={classNames(cls.ProjectPage, {}, [className])}>
-         <div className={cls.wrapper}>
-            <ProjectHeader className={cls.container} project={project} />
-            <ProjectLaptop className={cls.container} images={laptopImages} />
+         <div className={classNames('', {}, [cls.wrapper, cls.container])}>
+            <ProjectHeader project={project} />
+            <ProjectLaptop images={laptopImages} />
          </div>
-         <ProjectDevices project={project} />
-         <div style={{ height: '300vh' }}></div>
+         <ProjectDevices images={mobileImages} />
+         <div className={classNames('', {}, [cls.wrapper, cls.container])}>
+            <ProjectLaptop images={mainImages} tablet />
+         </div>
       </Page>
    );
 };
