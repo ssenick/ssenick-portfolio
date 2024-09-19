25679e5e3d0c2f2f8c8351c5648aca3f792b2a8d
ssenick <ssenick@ukr.net>
2024-09-18 20:30:29 -0600

    added ParallaxVideosGroupSliders

--- src/pages/HomePage/ui/HomeGallery/HomeGallery.tsx
+++ src/pages/HomePage/ui/HomeGallery/HomeGallery.tsx
@@ -1,39 +1,25 @@
-import { useScroll } from 'framer-motion';
-import { memo, useRef } from 'react';
+import { memo } from 'react';
 
-import ImageBlog from '@/assets/projects/blog/blog2.mp4';
-import ImageEurotex from '@/assets/projects/eurotex/eurotex2.mp4';
-import ImageGart from '@/assets/projects/gart/gart2.mp4';
-import ImageGreenShop from '@/assets/projects/greenshop/greenshop2.mp4';
-import ImageHelp from '@/assets/projects/help/help2.mp4';
-import ImagePortfolio from '@/assets/projects/portfolio/portfolio2.mp4';
-import ImageSportbet from '@/assets/projects/sportbet/sportbet2.mp4';
-import ImageStatika from '@/assets/projects/statika/statika2.mp4';
-import { ParallaxSlider } from '@/components/ParallaxSlider/ParallaxSlider';
-import { classNames } from '@/helpers/classNames/classNames';
-
-import cls from './HomeGallery.module.scss';
+import VideoBlog from '@/assets/projects/blog/video/video_1.mp4';
+import VideoEurotex from '@/assets/projects/eurotex/eurotex2.mp4';
+import VideoGart from '@/assets/projects/gart/gart2.mp4';
+import VideoGreenShop from '@/assets/projects/greenshop/greenshop2.mp4';
+import VideoHelp from '@/assets/projects/help/help2.mp4';
+import VideoPortfolio from '@/assets/projects/portfolio/portfolio2.mp4';
+import VideoSportbet from '@/assets/projects/sportbet/sportbet2.mp4';
+import VideoStatika from '@/assets/projects/statika/statika2.mp4';
+import { ParallaxVideosGroupSliders } from '@/components/ParallaxVideosGroupSliders/ParallaxVideosGroupSliders.tsx';
 
 interface HomeGalleryProps {
    className?: string;
 }
 
-const video1 = [ImageEurotex, ImageSportbet, ImagePortfolio, ImageStatika];
-const video2 = [ImageGart, ImageBlog, ImageHelp, ImageGreenShop];
+const video1: [string, string, string, string] = [VideoEurotex, VideoSportbet, VideoPortfolio, VideoStatika];
+const video2: [string, string, string, string] = [VideoGart, VideoBlog, VideoHelp, VideoGreenShop];
 
 const HomeGallery = memo((props: HomeGalleryProps) => {
    const { className } = props;
-   const ref = useRef<HTMLDivElement | null>(null);
-   const { scrollYProgress } = useScroll({
-      target: ref,
-      offset: ['start end', 'end start'],
-   });
-   return (
-      <div className={classNames(cls.HomeGallery, {}, [className])}>
-         <ParallaxSlider videos={video1} progress={scrollYProgress} />
-         <ParallaxSlider revers={true} videos={video2} progress={scrollYProgress} direction={'right'} />
-      </div>
-   );
+   return <ParallaxVideosGroupSliders className={className} videosOne={video1} videosTwo={video2} />;
 });
 
 HomeGallery.displayName = 'HomeGallery';
