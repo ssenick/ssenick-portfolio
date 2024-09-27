657eaeca15a357f7395c72fd7379b7611eb5b961
ssenick <ssenick@ukr.net>
2024-09-26 19:28:29 -0600

    changed video path name

--- src/pages/HomePage/ui/HomeGallery/HomeGallery.tsx
+++ src/pages/HomePage/ui/HomeGallery/HomeGallery.tsx
@@ -2,9 +2,9 @@ import { memo } from 'react';
 
 import VideoBlog from '@/assets/projects/blog/video/video.mp4';
 import VideoEurotex from '@/assets/projects/eurotex/video/video.mp4';
-import VideoGart from '@/assets/projects/gart/gart2.mp4';
+import VideoGart from '@/assets/projects/gart/video/video.mp4';
 import VideoGreenShop from '@/assets/projects/greenshop/video/video.mp4';
-import VideoHelp from '@/assets/projects/help/help2.mp4';
+import VideoHelp from '@/assets/projects/help/video/video.mp4';
 import VideoPortfolio from '@/assets/projects/portfolio/video/video.mp4';
 import VideoSportbet from '@/assets/projects/sportbet/video/video.mp4';
 import VideoStatika from '@/assets/projects/statika/video/video.mp4';
