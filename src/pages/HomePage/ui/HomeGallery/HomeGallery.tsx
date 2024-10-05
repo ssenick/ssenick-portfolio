919191b43cf3c82634e89c50e304ed9e56dff0f7
ssenick <ssenick@ukr.net>
2024-10-04 20:10:42 -0600

    fixed path to videos

--- src/pages/HomePage/ui/HomeGallery/HomeGallery.tsx
+++ src/pages/HomePage/ui/HomeGallery/HomeGallery.tsx
@@ -1,13 +1,13 @@
 import { memo } from 'react';
 
-import VideoBlog from '@/assets/projects/blog/video/video.mp4';
-import VideoEurotex from '@/assets/projects/eurotex/video/video.mp4';
-import VideoGart from '@/assets/projects/gart/video/video.mp4';
-import VideoGreenShop from '@/assets/projects/greenshop/video/video.mp4';
-import VideoHelp from '@/assets/projects/help/video/video.mp4';
-import VideoPortfolio from '@/assets/projects/portfolio/video/video.mp4';
-import VideoSportbet from '@/assets/projects/sportbet/video/video.mp4';
-import VideoStatika from '@/assets/projects/statika/video/video.mp4';
+import VideoBlog from '@/assets/videos/blog.mp4';
+import VideoEurotex from '@/assets/videos/eurotex.mp4';
+import VideoGart from '@/assets/videos/gart.mp4';
+import VideoGreenShop from '@/assets/videos/greenshop.mp4';
+import VideoHelp from '@/assets/videos/help.mp4';
+import VideoPortfolio from '@/assets/videos/portfolio.mp4';
+import VideoSportbet from '@/assets/videos/sportbet.mp4';
+import VideoStatika from '@/assets/videos/statika.mp4';
 import { ParallaxVideosGroupSliders } from '@/components/ParallaxVideosGroupSliders/ParallaxVideosGroupSliders.tsx';
 
 interface HomeGalleryProps {
