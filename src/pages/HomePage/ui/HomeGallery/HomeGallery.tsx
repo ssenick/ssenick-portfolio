fab7e7830fc8fb856411cacb5430966ad8fae45a
ssenick <ssenick@ukr.net>
2024-10-09 18:50:31 -0600

    created new video constants

--- src/pages/HomePage/ui/HomeGallery/HomeGallery.tsx
+++ src/pages/HomePage/ui/HomeGallery/HomeGallery.tsx
@@ -1,25 +1,45 @@
 import { memo } from 'react';
 
 import VideoBlog from '@/assets/videos/blog.mp4';
+import ImageBlog from '@/assets/videos/blog.webp';
 import VideoEurotex from '@/assets/videos/eurotex.mp4';
+import ImageEurotex from '@/assets/videos/eurotex.webp';
 import VideoGart from '@/assets/videos/gart.mp4';
+import ImageGart from '@/assets/videos/gart.webp';
 import VideoGreenShop from '@/assets/videos/greenshop.mp4';
+import ImageGreenShop from '@/assets/videos/greenshop.webp';
 import VideoHelp from '@/assets/videos/help.mp4';
+import ImageHelp from '@/assets/videos/help.webp';
 import VideoPortfolio from '@/assets/videos/portfolio.mp4';
+import ImagePortfolio from '@/assets/videos/portfolio.webp';
 import VideoSportbet from '@/assets/videos/sportbet.mp4';
+import ImageSportbet from '@/assets/videos/sportbet.webp';
 import VideoStatika from '@/assets/videos/statika.mp4';
+import ImageStatika from '@/assets/videos/statika.webp';
 import { ParallaxVideosGroupSliders } from '@/components/ParallaxVideosGroupSliders/ParallaxVideosGroupSliders.tsx';
+import type { VideoGroup } from '@/types/videoGroup.ts';
 
 interface HomeGalleryProps {
    className?: string;
 }
 
-const video1: [string, string, string, string] = [VideoEurotex, VideoSportbet, VideoPortfolio, VideoStatika];
-const video2: [string, string, string, string] = [VideoGart, VideoBlog, VideoHelp, VideoGreenShop];
-
+const videoGroupOne: VideoGroup[] = [
+   { video: VideoEurotex, previewImage: ImageEurotex },
+   { video: VideoSportbet, previewImage: ImageSportbet },
+   { video: VideoPortfolio, previewImage: ImagePortfolio },
+   { video: VideoStatika, previewImage: ImageStatika },
+];
+const videoGroupTwo: VideoGroup[] = [
+   { video: VideoGart, previewImage: ImageGart },
+   { video: VideoBlog, previewImage: ImageBlog },
+   { video: VideoHelp, previewImage: ImageHelp },
+   { video: VideoGreenShop, previewImage: ImageGreenShop },
+];
 const HomeGallery = memo((props: HomeGalleryProps) => {
    const { className } = props;
-   return <ParallaxVideosGroupSliders className={className} videosOne={video1} videosTwo={video2} />;
+   return (
+      <ParallaxVideosGroupSliders className={className} groupOne={videoGroupOne} groupTwo={videoGroupTwo} />
+   );
 });
 
 HomeGallery.displayName = 'HomeGallery';
