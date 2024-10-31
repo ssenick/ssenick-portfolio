59c70fbce86df61ebe0f76e9ff209a7c9dd8767b
ssenick <ssenick@ukr.net>
2024-10-30 18:13:47 -0600

    added location prop and used in getPageName

--- src/poviders/PageTransition/ui/PageTransition/PageTransition.tsx
+++ src/poviders/PageTransition/ui/PageTransition/PageTransition.tsx
@@ -17,31 +17,37 @@ interface PageTransitionProps {
    className?: string;
    children?: ReactNode;
    location: Location;
+   inited: boolean;
 }
 
 const PageTransition = memo((props: PageTransitionProps) => {
-   const { className, children, location } = props;
+   const { className, children, location, inited } = props;
    const { width, height } = useDimensions();
 
-   const getPageName = useCallback((path: string) => {
-      const cleanPath = path.endsWith('/') && path !== '/' ? path.slice(0, -1) : path;
+   const getPageName = useCallback(
+      (path: string) => {
+         const cleanPath = path.endsWith('/') && path !== '/' ? path.slice(0, -1) : path;
 
-      const values = Object.values(AppRoutes).find((route) => {
-         const filterValue = routeConfig[route]?.path?.split('')?.includes('/:');
-         if (!filterValue) return routeConfig[route]?.path === cleanPath;
-      });
-
-      if (values) {
-         return values;
-      } else {
-         const finderValue = projects?.find((project) => project.path === path.split('/')[2]);
-         if (finderValue) {
-            return finderValue.path;
-         } else {
-            return 'Not found page';
+         const values = Object.values(AppRoutes).find((route) => {
+            const filterValue = routeConfig[route]?.path?.split('')?.includes('/:');
+            if (!filterValue) return routeConfig[route]?.path === cleanPath;
+         });
+         if (inited) {
+            return 'RUSLAN SENCHENKO';
          }
-      }
-   }, []);
+         if (values) {
+            return values;
+         } else {
+            const finderValue = projects?.find((project) => project.path === path.split('/')[2]);
+            if (finderValue) {
+               return finderValue.path;
+            } else {
+               return 'Not found page';
+            }
+         }
+      },
+      [inited],
+   );
 
    return (
       <AnimatePresence mode="wait">
