bf855352936e7384354b5b0aee58410104b7df26
ssenick <ssenick@ukr.net>
2024-10-30 18:12:53 -0600

    deleted location

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,14 @@
 import './styles/index.scss';
 
 import { Suspense } from 'react';
-import { useLocation } from 'react-router-dom';
 
 import { classNames } from '@/helpers/classNames/classNames';
 import { AppRouter } from '@/poviders/Router';
 function App() {
-   const location = useLocation();
-
    return (
       <div className={classNames('app', {}, [])}>
          <Suspense fallback={''}>
-            <AppRouter location={location} />
+            <AppRouter />
          </Suspense>
       </div>
    );
