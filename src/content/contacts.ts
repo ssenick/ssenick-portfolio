901a85f77becb658691a9ca6cd87c6d110edd0f5
ssenick <ssenick@ukr.net>
2024-12-04 12:32:57 +0100

    chore: added new contact number contacts.ts

--- src/content/contacts.ts
+++ src/content/contacts.ts
@@ -1,6 +1,6 @@
 import type { ILinksList } from '@/components/LinksList/LinksList.tsx';
 
-export const PHONE = '+380668049554';
+export const PHONE = '+48503638883';
 export const EMAIL = 'ssenick@ukr.net';
 
 export const contacts: ILinksList[] = [
