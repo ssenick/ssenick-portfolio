d015adf9f856ee6c9fcbc16e934f8f5044c11644
ssenick <ssenick@ukr.net>
2024-12-16 13:59:05 +0100

    chore: added React Google Analytics 4

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,15 @@
 import './styles/index.scss';
 
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
+import ReactGA from 'react-ga4';
 
 import { classNames } from '@/helpers/classNames/classNames';
 import { AppRouter } from '@/poviders/Router';
 function App() {
+   useEffect(() => {
+      ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_KEY);
+   }, []);
+
    return (
       <div className={classNames('app', {}, [])}>
          <Suspense fallback={''}>
